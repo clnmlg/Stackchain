d0d23d63ec9a5f7dcafa6f6c063148ee71dd124a
c1nm1g <colin.mlg@posteo.net>
2022-06-17 11:46:39 +0200

    fix: darkmode by default + redirection auth

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import App from './App'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import {
     legacy_createStore as createStore,
@@ -13,12 +13,18 @@ import reducers from './redux/reducers'
 import './index.css'
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+}
+const theme = extendTheme({ config })
+
 const container = document.getElementById('root')
 const root = createRoot(container!)
 
 root.render(
     <Provider store={store}>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App />
         </ChakraProvider>
     </Provider>
